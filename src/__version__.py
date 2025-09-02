2ed454a9bd5de43add6ede2623028599df8fab75
GitHub Action <action@github.com>
2025-09-02 22:24:55 +0000

    chore: auto-update version and build metadata [skip ci]

--- src/__version__.py
+++ src/__version__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Version information for PCILeech Firmware Generator."""
 
-__version__ = "0.13.11"
-__version_info__ = (0, 13, 11)
+__version__ = "0.13.12"
+__version_info__ = (0, 13, 12)
 
 # Release information
 __title__ = "PCILeech Firmware Generator"
@@ -13,5 +13,5 @@ __license__ = "MIT"
 __url__ = "https://github.com/voltcyclone/PCILeechFWGenerator"
 
 # Build metadata
-__build_date__ = "2025-09-02T22:13:43.854585"
-__commit_hash__ = "0d5479a"
+__build_date__ = "2025-09-02T22:24:54.986275"
+__commit_hash__ = "b786bca"
